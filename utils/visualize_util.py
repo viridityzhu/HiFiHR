c33f9ba87b405ffa9a9e66076a2feea942bed623
viridityzhu <zhujiayin@u.nus.edu>
2023-04-04 22:15:58 +0800

    fix: save mano obj correctly

--- utils/visualize_util.py
+++ utils/visualize_util.py
@@ -7,6 +7,7 @@ from rich import print
 
 import torch.nn as nn
 from torch.nn import functional as F
+from pytorch3d.io import IO
 
 import numpy as np
 import os
@@ -20,9 +21,12 @@ def displaydemo(mode_train, obj_output, image_output, epoch, idx, vertices, face
         # save 3d obj demo
         if skin_meshes is not None:
             demo_path = os.path.join(obj_output, 'demo{}_{:04d}_{:07d}.obj'.format(evalName, epoch, 0))
-            skin_v_smooth = skin_meshes.verts_padded()[0].detach().cpu().numpy()
-            # save_textured_nimble(demo_path, skin_v_smooth, textures[0].detach().cpu().numpy(), console=console)
-            save_textured_nimble(demo_path, skin_v_smooth, console=console)
+            if nimble_j2d is not None: # means it's nimble hand
+                skin_v_smooth = skin_meshes.verts_padded()[0].detach().cpu().numpy()
+                # save_textured_nimble(demo_path, skin_v_smooth, textures[0].detach().cpu().numpy(), console=console)
+                save_textured_nimble(demo_path, skin_v_smooth, console=console)
+            else:
+                IO().save_mesh(skin_meshes[0], demo_path)
 
     # save display img
     file_str = os.path.join(image_output, '{:04d}_{:07d}{}.png'.format(epoch, idx, evalName))
